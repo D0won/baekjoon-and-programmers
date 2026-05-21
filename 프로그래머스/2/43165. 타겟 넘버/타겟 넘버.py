e8f40e59ae5c893abe4b7dbb8b814b406a671115
Dowon Lee <ldwlee03@gmail.com>
2026-05-21 20:03:17 +0900

    [level 2] Title: 타겟 넘버, Time: 257.91 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,6 +1,7 @@
 def solution(numbers, target):
-    answer = 0
+    # 더하고 뺀 결과를 담는 리스트
     result = []
+    # dfs를 활용하여 numbers 안의 수가 - 혹은 +로 재귀를 통해 더하여 index가 넘으면 반환
     def dfs(numbers, idx, snum) :
         if idx >= len(numbers) :
             result.append(snum)
@@ -10,4 +11,5 @@ def solution(numbers, target):
         dfs(numbers, idx+1, pnum)
         dfs(numbers, idx+1, mnum)    
     dfs(numbers, 0, 0)
+    # result 리스트 안의 target의 수를 반환
     return result.count(target)
