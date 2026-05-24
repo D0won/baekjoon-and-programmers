82e8ffd158f7ac6d31e48c1d7d54f4f812097038
Dowon Lee <ldwlee03@gmail.com>
2026-05-24 15:24:47 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,13 +1,16 @@
+# ai 도움을 받음
 def solution(n, computers):
     answer = 0
+    # 방문한 컴퓨터 확인
     visited = [0] * n
     def dfs(computer) :
+        # 방문 시 해당 컴퓨터 번호를 1로 둠
         visited[computer] = 1
-        
+        # 해당 컴퓨터가 다른 컴퓨터와 연결되어 있는지, 그리고 해당 컴퓨터가 방문했는지 확인
         for j in range(n) :
             if computers[computer][j] == 1 and visited[j] == 0 :
                 dfs(j)
-        
+    # 방문한 적 있는 컴퓨터를 제외한 나머지 경우엔 answer += 1함.
     for i in range(n) :
         if visited[i] == 0 :
             dfs(i)
