643e15bd020693982f46ee5823350ba64c271a81
Dowon Lee <ldwlee03@gmail.com>
2026-09-09 15:41:56 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 2.70 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a+b;
+    return 0;
+}
