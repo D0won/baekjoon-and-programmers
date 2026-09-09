c359a3661331fadf497cdf196054231d7c8fef6a
Dowon Lee <ldwlee03@gmail.com>
2026-09-09 15:22:19 +0900

    [level 0] Title: a와 b 출력하기, Time: 2.31 ms, Memory: 4.57 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << "a = " << a << "\n";
+    cout << "b = " << b << "\n";
+    return 0;
+}
